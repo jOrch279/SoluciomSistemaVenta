5457056f8681fd692cc3fb83558f76e2ff9a8b80
jorge pascual lugardo <jorgelugardo3@outlook.com>
2023-06-15 13:17:29 -0600

    Implementar interfaz

---
+++ SistemaVenta.DAL/Implementacion/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Implementacion
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        public Task<IQueryable<TEntity>> Consultar(Expression<Func<TEntity, bool>> filtro = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Crear(TEntity entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Editar(TEntity entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(TEntity entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filtro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
